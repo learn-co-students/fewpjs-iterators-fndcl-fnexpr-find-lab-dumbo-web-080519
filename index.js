5630d8d7274962f1d95d39391b348af08a1c6626
mrwerner392 <mrwerner392@gmail.com>
2019-09-23 12:52:40 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrOfDataObjs) {
+  let foundDataObj = arrOfDataObjs.find(dataObj => dataObj.result === "W");
+  return foundDataObj ? foundDataObj.year : undefined;
+}
